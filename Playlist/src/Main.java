20df05adf3ee745bd7ce729164b0d2a54ecb4490
Nikolay Petrov <nikolay.petrov.petrov.97@gmail.com>
2020-11-03 00:00:56 +0200

    Playlist Part 4

--- Playlist/src/Main.java
+++ Playlist/src/Main.java
@@ -27,8 +27,10 @@ public class Main {
         //str1 по азбучен ред идва преди str2. Положително число означава, че str1 идва по азбучен ред след str2
 
         boolean quit = false;
+        boolean quitPlaylist = false;
         boolean isInt;
         int choice;
+        int choicePlaylist;
 
 
         printMenu();
@@ -66,6 +68,40 @@ public class Main {
                     break;
                 case 6:
                     goToPlaylistMenu();
+
+                    while (!quitPlaylist) {
+                        System.out.println("Choose an option: ");
+                        isInt = scanner.hasNextInt();
+                        if (isInt) {
+
+                            choicePlaylist = scanner.nextInt();
+                            scanner.nextLine();
+                        } else {
+
+                            System.out.println("Invalid choice.");
+                            choicePlaylist = -1;
+                        }
+
+                        switch (choicePlaylist) {
+
+                            case 1:
+                                //todo add a playlist
+                            case 2:
+                                //todo add a song to playlist
+                            case 3:
+                                //todo play a playlist
+                            case 4:
+                                //todo print playlist
+                            case 5:
+                                goToPlaylistMenu();
+                                break;
+                            default:
+                                System.out.println("Exiting playlist menu.");
+                                quitPlaylist = true;
+                                break;
+                        }
+
+                    }
                     break;
                 default:
                     System.out.println("Exiting program");
@@ -97,7 +133,9 @@ public class Main {
         System.out.println("1. Add a playlist");
         System.out.println("2. Add a song to a playlist");
         System.out.println("3. Play a playlist");
-        System.out.println("4. Quit");
+        System.out.println("4. Print playlist");
+        System.out.println("5. Print playlist menu");
+        System.out.println("6. Quit");
 
         // TODO: 2.11.2020 г. continue
     }
