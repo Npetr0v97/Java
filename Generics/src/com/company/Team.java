198149666fb9d932ebcaa4324d55f1d3ce89b3a9
Nikolay Petrov <nikolay.petrov.petrov.97@gmail.com>
2020-11-24 01:42:54 +0200

    tt

--- Generics/src/com/company/Team.java
+++ Generics/src/com/company/Team.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T> {
 
     private String name;
     int played = 0;
@@ -10,7 +10,7 @@ public class Team {
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members =  new ArrayList<>();
+    private ArrayList<T> members =  new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -20,16 +20,16 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
 
         if (members.contains(player)) {
 
-            System.out.println(player.getName() + " is already on this team.");
+            System.out.println(((Player) player).getName() + " is already on this team.");
             return false;
         } else {
 
             members.add(player);
-            System.out.println(player.getName() + " picked for team " + this.name);
+            System.out.println(((Player) player).getName() + " picked for team " + this.name);
             return true;
         }
     }
