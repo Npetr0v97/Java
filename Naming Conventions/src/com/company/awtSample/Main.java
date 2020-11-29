08290032fb649193f0a4bc440f96ff3319048092
Nikolay Petrov <nikolay.petrov.petrov.97@gmail.com>
2020-11-29 15:36:48 +0200

    packagesPart1

---
+++ Naming Conventions/src/com/company/awtSample/Main.java
@@ -0,0 +1,13 @@
+package com.company.awtSample;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+
+    }
+}
