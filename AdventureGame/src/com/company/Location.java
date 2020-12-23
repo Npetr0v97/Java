220549268b9b5d82607cdfe68c80467c1884e6ce
Npetr0v97 <nikolay.petrov.petrov.97@gmail.com>
2020-12-23 13:32:21 +0200

    AdventureGameChallenge

--- AdventureGame/src/com/company/Location.java
+++ AdventureGame/src/com/company/Location.java
@@ -6,19 +6,25 @@ import java.util.Map;
 public class Location {
 
     private final int locationID;
+    private final String name;
     private final String description;
-    private final Map<String, Integer> exits;
+    private final Map<Integer, String> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String name, String description) {
         this.locationID = locationID;
+        this.name = name;
         this.description = description;
 
-        exits = new HashMap<String, Integer>();
+        exits = new HashMap<Integer, String>();
     }
 
-    public void addExit(String direction, int location) {
 
-        exits.put(direction,location);
+    public void addExit(int location, String direction) {
+        exits.put(location, direction);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getLocationID() {
@@ -29,8 +35,8 @@ public class Location {
         return description;
     }
 
-    public Map<String, Integer> getExits() {
-        return new HashMap<String, Integer>(exits);
+    public Map<Integer,String> getExits() {
+        return new HashMap<Integer,String>(exits);
         //това се използва за върне копие на exits. По този начин оригиналния exits не може да бъде променен от външна страна;
     }
 }
--- AdventureGame/src/com/company/Main.java
+++ AdventureGame/src/com/company/Main.java
@@ -9,12 +9,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0,"You are sitting in front of the computer learning Java"));
-        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2,"You are at the top of a hill"));
-        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4,"You are in a valley beside a stream"));
-        locations.put(5, new Location(5,"You are in the forest"));
+        locations.put(1, new Location(1, "Road","You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2,"Hill","You are at the top of a hill"));
+        locations.put(3, new Location(3,"Building","You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4,"Valley","You are in a valley beside a stream"));
+        locations.put(5, new Location(5,"Forest","You are in the forest"));
+
+        // TODO: 23.12.2020 г. Add the directions for all locations
+
+        // TODO: 23.12.2020 г. Do while flow with choices 1. Travel / 2. Quit 
+
+        // TODO: 23.12.2020 г. Display available directions with the locations in brackets
+        //Current location: Road
+        //1. North (Forest)
+        //2. East (Building)
+        //3. South (Valley)
+        //4. West (Hill)
 
         int loc = 1;
 
