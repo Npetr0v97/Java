4b24033394ff9fa946dc4ccc254fbd028c40f229
Nikolay Petrov <nikolay.petrov.petrov.97@gmail.com>
2021-01-11 23:47:57 +0200

    ToDoApp pt2

--- ToDoApp/src/com/company/Label.java
+++ ToDoApp/src/com/company/Label.java
@@ -1,6 +1,47 @@
 package com.company;
 
-public class Label {
+import java.util.Objects;
 
-    // TODO: 11.1.2021 г. idName + name
+public class Label implements Comparable<Label>{
+    private static int counter = 1;
+    private final int id;
+    private final String name;
+
+    public Label(String name) {
+        this.name = name;
+        this.id = counter;
+        counter++;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Label label = (Label) o;
+        return name.equals(label.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name) + 123;
+    }
+
+    @Override
+    public int compareTo(Label label) {
+        if (this.name.equals(label.getName())) {
+            return 0;
+        }
+
+        return  this.name.compareTo(label.getName());
+
+    }
 }
--- ToDoApp/src/com/company/Task.java
+++ ToDoApp/src/com/company/Task.java
@@ -1,8 +1,75 @@
 package com.company;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 public class Task {
-    // TODO: 11.1.2021 г. task name + boolean finished + due time (date + time) + notification type e_num + CRON expression +
-    // TODO: 11.1.2021 г. + labels (LinkedHashMap) <Label, Description>
+
+
+
+    public enum NotificationType {
+        vibration,
+        sound,
+        vibrationAndSound
+    }
+    private final String name;
+    private boolean finished;
+    private String dueDate;
+    private String dueTime;
+    private String repeat;
+    private String notificationType;
+    private Map<Label, String> labels;
+
+    public Task(String name, String dueDate, String dueTime, String repeat, NotificationType notificationType) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.dueTime = dueTime;
+        this.repeat = repeat;
+        this.labels = new LinkedHashMap<>();
+        switch (notificationType) {
+            case sound:
+                this.notificationType = "Sound only";
+                break;
+            case vibration:
+                this.notificationType = "Vibration only";
+                break;
+            case vibrationAndSound:
+                this.notificationType = "Vibration and sound";
+                break;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getDueTime() {
+        return dueTime;
+    }
+
+    public String getRepeat() {
+        return repeat;
+    }
+
+    public String getNotificationType() {
+        return notificationType;
+    }
+
+    public Map<Label, String> getLabels() {
+        return Collections.unmodifiableMap(labels);
+    }
+
+
+
+
 }
