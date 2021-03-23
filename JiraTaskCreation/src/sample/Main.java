7f5671707e538a53dd09620262a5d25c829be162
Npetr0v97 <nikolay.petrov.petrov.97@gmail.com>
2021-03-23 15:46:19 +0200

    JiraIssues

--- JiraTaskCreation/src/sample/Main.java
+++ JiraTaskCreation/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Jira Issue Creation Screen");
-        primaryStage.setScene(new Scene(root, 800, 475));
+        primaryStage.setScene(new Scene(root, 720, 575));
         primaryStage.show();
     }
 
