7de048d5f53f3b7a6ea16ca786f1f03e62c22991
Nikolay Petrov <nikolay.petrov.petrov.97@gmail.com>
2021-05-29 23:51:53 +0300

    prod 1

--- mockLoadingsApp/src/com/example/seriesFunctions/Main.java
+++ mockLoadingsApp/src/com/example/seriesFunctions/Main.java
@@ -1,13 +1,18 @@
 package com.example.seriesFunctions;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
 
 //        System.out.println("Hello World");
+
+        boolean exit = false;
 
 
         String text = "Здравейте. желая да открия товарителница 105102103456.\n" +
@@ -15,18 +20,71 @@ public class Main {
 
         System.out.println("Old text: \n" + text);
 
-        RegexToolFinal.addLoading(53001234567891l);
-        RegexToolFinal.addLoading(105102103456l);
-        RegexToolFinal.addLoading(1051021034512l);
-        RegexToolFinal.addLoading(1051021034533l);
+        RegexToolFinal tool = new RegexToolFinal();
 
-        RegexToolFinal.printLoadings();
+//        tool.addLoading(53001234567891l);
+//        tool.addLoading(105102103456l);
+//        tool.addLoading(1051021034512l);
+//        tool.addLoading(1051021034533l);
 
-        RegexToolFinal.searchLoadings(text);
+
+
+        tool.printLoadings();
+
+        tool.searchLoadings(text);
+
+        while (!exit) {
+            int choice;
+            printMenu();
+
+            try {
+                choice = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Exiting program");
+                exit = true;
+                break;
+            }
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("What is the loading number: ");
+                    try {
+                       tool.addLoading(in.nextLong());
+                       tool.cleanUp();
+                    } catch (InputMismatchException f) {
+                        f.printStackTrace();
+                        System.out.println("Invalid number format.");
+                    }
+                    break;
+                case 2:
+                    System.out.print("What loading are you searching for: ");
+                    try {
+                        System.out.println(tool.returnLoadingURL(in.nextLong()));
+                    } catch (InputMismatchException f) {
+                        f.printStackTrace();
+                        System.out.println("Invalid number format.");
+                    }
+                    break;
+                default:
+                    System.out.println("Exiting program");
+                    exit = true;
+            }
+        }
+
+        tool.cleanUp();
 
 //        ArrayList<String> loadings = RegexTool.searchLoadings(text);
 
        // System.out.println("New text: \n" + RegexTool.searchLoadings(text));
+
+    }
+
+
+    private static void printMenu() {
+
+
+        System.out.print("MENU---------------\n1. Add a Loading\n2. Receive URL\n3. Exit\nMake a choice: ");
 
     }
 }
