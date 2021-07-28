5b67f43766b96a00efb9e5fd324fa14edba8b8be
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2021-07-29 00:17:14 +0300

    exceptions part  3

--- new-pc-2/src/com/company/Example.java
+++ new-pc-2/src/com/company/Example.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
@@ -11,19 +13,41 @@ public class Example {
     }
 
     private static int divide() {
+        int x,y;
+        try {
+            x = getInt();
+            y = getInt();
 
-        int x = getInt();
-        int y = getInt();
+        } catch (NoSuchElementException e) {
+
+            throw new ArithmeticException("no suitable input");
+        }
         System.out.println("x is " + x +", y is " + y);
 
-        return x/y;
+
+        try {
+            return x/y;
+
+        } catch (NoSuchElementException e) {
+
+            throw new ArithmeticException("no suitable input");
+        }
+
     }
 
     private static int getInt() {
 
         Scanner s = new Scanner(System.in);
         System.out.println("Enter an integer");
+        while (true) {
+            try {
+                return s.nextInt();
 
-        return s.nextInt();
+            } catch (InputMismatchException e) {
+                s.nextLine();
+                System.out.println("Please enter a number using only digits 0 to 9");
+            }
+
+        }
     }
 }
