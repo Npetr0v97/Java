12cb89295a780242bc773320ab00526723b03fe0
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2021-11-04 00:59:12 +0200

    problem6

---
+++ ProjectEuler/src/com/company/Problem6.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Problem6 {
+
+    public static void main(String[] args) {
+
+
+        int answer=0;
+
+        int sqSum=0,sumSq=0;
+
+        for (int i=1; i<=100; i++) {
+
+            sumSq+=i*i;
+            sqSum+=i;
+
+            System.out.println("sumSq="+sumSq);
+            System.out.println("sqSum="+sqSum);
+        }
+
+        answer=sqSum*sqSum-sumSq;
+
+        System.out.println(answer);
+    }
+}
