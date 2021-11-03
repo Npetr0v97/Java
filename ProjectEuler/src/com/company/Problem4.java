4b6b781bbe93882b741f4c0cad47d814975a2c4c
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2021-11-03 19:58:59 +0200

    ,

--- ProjectEuler/src/com/company/Problem4.java
+++ ProjectEuler/src/com/company/Problem4.java
@@ -6,34 +6,105 @@ public class Problem4 {
 
     public static void main(String[] args) {
 
+/*        A palindromic number reads the same both ways. The largest palindrome made
+            from the product of two 2-digit numbers is 9009 = 91 × 99.
 
-        int min=100*100;
-        int max = 999*999;
+        Find the largest palindrome made from the product of two 3-digit numbers.*/
+
+
         int counter=0;
         int answer=0;
+        ArrayList<Integer> palindromes = new ArrayList<>();
 
-        ArrayList<Integer> numbers = new ArrayList<>();
+        boolean flip=true;
+        int x = 999,y=999;
 
-        System.out.println("max "+max);
+        for (int i=999; i>=100; i--) {
 
-        for (int i = max; i >= min; i--) {
+            for (int j=999; j>=100; j--) {
+                counter++;
 
-            counter++;
-            if (isPalindrome(i)){
-                answer=i;
-                break;
+//                System.out.println(counter);
+
+
+                if (isPalindrome(i*j)) {
+
+                    palindromes.add(i * j);
+
+                }
+            }
+        }
+//        while (true) {
+//            counter++;
+//            System.out.println(x+"*"+y+"="+(x*y));
+//            if (isPalindrome(x*y)) {
+//
+//                palindromes.add(x * y);
+//
+//            }
+//
+//
+//            if (flip) {
+//
+//                x--;
+//                flip=false;
+//            } else {
+//
+//                y--;
+//                flip=true;
+//            }
+//
+//            if (x<100 && y<100) {
+//                System.out.println("Out of numbers");
+//                break;
+//            }
+//        }
+        if (palindromes.isEmpty()) {
+
+            System.out.println("Empty array");
+        } else {
+            answer=palindromes.get(0);
+            System.out.println(palindromes);
+
+            for (int i=1; i<palindromes.size(); i++) {
+
+                if (answer<palindromes.get(i)) {
+
+                    answer = palindromes.get(i);
+                }
             }
 
+            System.out.println("Answer: " + answer);
+            System.out.println(counter + " cycles");
         }
 
-        System.out.println("Answer: "+ answer);
-        System.out.println(counter+ " cycles");
+
 
 
     }
 
     private static boolean isPalindrome (int x) {
-        boolean result=false;
+        boolean result=true;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (true) {
+
+            if (x!=0) {
+                numbers.add(x%10);
+                x=x/10;
+            } else {
+                break;
+            }
+
+        }
+
+        for (int i=0; i<numbers.size()/2; i++) {
+
+            if (numbers.get(i)!=numbers.get(numbers.size()-1-i)){
+                result=false;
+                break;
+            }
+        }
 
 
         return result;
