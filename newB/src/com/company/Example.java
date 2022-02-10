48cbe61f847c37c355ba1adfc41f6c450a1785bb
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2022-02-11 01:16:07 +0200

    67

--- newB/src/com/company/Example.java
+++ newB/src/com/company/Example.java
@@ -7,29 +7,34 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
+
     }
 
     private static  int divide() {
         int x,y;
         try {
             x = getInt();
-            y=getInt();
+            y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x / y;
 
 
         } catch (NoSuchElementException e) {
             throw new ArithmeticException("no suitable input");
 
-        }
-        System.out.println("x is " + x + ", y is " + y);
-
-        try {
-            return x / y;
-
         } catch (ArithmeticException e) {
             throw new ArithmeticException("division by 0");
+
         }
+
     }
 
     private static int getInt() {
